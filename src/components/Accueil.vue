1ee72deba06b070f4c0256494d0218f643d20aac
Beowulf-0 <73909294+Beowulf-0@users.noreply.github.com>
2022-12-02 02:39:56 +0100

    Update Accueil.vue, base et main.css

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -1,40 +1,74 @@
 <!-- Frame 4->6 -->
 <template>
 
-  <main>
-    <div>
+  <div id="corps">
+    <section>
+      <h1> Titre de la section </h1>
+      <article>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Eros donec ac odio tempor orci. Convallis convallis tellus id interdum velit laoreet.
+          Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Odio eu feugiat pretium nibh ipsum consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Eros donec ac odio tempor orci. Convallis convallis tellus id interdum velit laoreet.
+        </p>
+      </article>
+      <article class="art">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Enim lobortis scelerisque fermentum dui faucibus in ornare quam.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Eros donec ac odio tempor orci. Convallis convallis tellus id interdum velit laoreet.
+        </p>
+        <div class="bloc"></div>
+      </article>
+      <article>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Eros donec ac odio tempor orci. Convallis convallis tellus id interdum velit laoreet.
+          Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Odio eu feugiat pretium nibh ipsum consequat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Eros donec ac odio tempor orci. Convallis convallis tellus id interdum velit laoreet.
+        </p>
+      </article>
+    </section>
 
-      <section>
-        <img class="img" src="image.png">
-        <p class="txt"> Lorem ipsum</p>
-      </section>
 
-      <section class="art">
-        <h2> Titre de la section </h2>
-        <article>
-
-          <section class="test">
-            <div>
-              <h3> Titre de la section </h3>
-              <p>contenuuuuuuuuuu</p>
-            </div>
-            <div>
-              <img class="img_mascotte" src="../assets/lopotichat_beau.svg">
-            </div>
-          </section>
-          <p> lorem ipsum </p>
-
-          <section class="test">
+    <!--<section>
+      <h2> Titre de la section </h2>
+      <article>
+        <section class="test">
+          <div>
+            <p> Contenuuuuu </p>
+          </div>
+          <div>
             <img class="img_mascotte" src="../assets/lopotichat_beau.svg">
-            <p> "Citation" </p>
-          </section>
-        </article>
+          </div>
+        </section>
+      </article>
 
+      <p> Lorem ipsum </p>
+    </section>
 
-      </section>
+    <section>
+      <h2> Titre de la section </h2>
+      <article>
+        <section class="test">
+          <div>
+            <img class="img_mascotte" src="../assets/lopotichat_beau.svg">
 
-    </div>
-  </main>
+          </div>
+          <div>
+            <p>"Citation"</p>
+          </div>
+        </section>
+      </article>
+
+      <p> Lorem ipsum </p>
+    </section>-->
+
+  </div>
+
 </template>
 
 <script>
@@ -44,33 +78,45 @@ export default {
 </script>
 
 <style scoped>
-  .txt{
-
+  #corps{
+    width:90vw;
+    margin: 0 auto;
   }
 
-  .img{
-    display: flex;
-    align-content: center;
+  article p{
+    margin: 8px 5px;
+  }
 
-    width:5vw;
-    height:5vh;
+  .art{
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .bloc{
+    display: block;
+    background-color: red;
+    width: 200px;
+
   }
 
   .test{
     display: flex;
+    justify-content: space-between;
 
     border-style: solid;
     border-radius: 12px;
     border-color: white;
 
-    justify-content: space-between;
     padding: 10px 20px 10px 20px;
+    margin-top: 20px;
+    margin-bottom: 20px;
   }
 
   .img_mascotte{
     width: 20vw;
     height: 20vw;
   }
+
 
 
 </style>
