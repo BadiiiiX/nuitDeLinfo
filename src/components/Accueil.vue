c6fc948e3b440bf0f18d76ed810686177d2ccce0
Beowulf-0 <73909294+Beowulf-0@users.noreply.github.com>
2022-12-02 03:04:37 +0100

    Réorganisation du html et du style de Accueil.vue

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -13,14 +13,13 @@
           Eros donec ac odio tempor orci. Convallis convallis tellus id interdum velit laoreet.
         </p>
       </article>
-      <article class="art">
+      <article class="flex">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Enim lobortis scelerisque fermentum dui faucibus in ornare quam.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Eros donec ac odio tempor orci. Convallis convallis tellus id interdum velit laoreet.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra orci sagittis eu volutpat odio vitae ultricies leo integer malesuada.
+          Consectetur purus ut faucibus pulvinar. Turpis egestas maecenas pharetra convallis posuere morbi. Molestie at elementum eu facilisis sed odio morbi quis.
+          Consectetur purus ut faucibus pulvinar. Turpis egestas maecenas pharetra convallis posuere morbi. Molestie at elementum eu facilisis sed odio morbi quis.
         </p>
-        <div class="bloc"></div>
+        <div class="bloc"></div> <!-- A remplacer par l'image à animer -->
       </article>
       <article>
         <p>
@@ -34,38 +33,34 @@
     </section>
 
 
-    <!--<section>
+    <section class="sect">
       <h2> Titre de la section </h2>
-      <article>
-        <section class="test">
-          <div>
-            <p> Contenuuuuu </p>
-          </div>
-          <div>
-            <img class="img_mascotte" src="../assets/lopotichat_beau.svg">
-          </div>
-        </section>
+      <article class="test">
+        <div>
+          <p> Contenuuuuu </p>
+        </div>
+        <div>
+          <img class="img_mascotte" src="../assets/lopotichat_beau.svg">
+        </div>
       </article>
-
-      <p> Lorem ipsum </p>
     </section>
 
-    <section>
-      <h2> Titre de la section </h2>
-      <article>
-        <section class="test">
-          <div>
-            <img class="img_mascotte" src="../assets/lopotichat_beau.svg">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Viverra orci sagittis eu volutpat odio. Mauris vitae ultricies leo integer malesuada purus ut faucibus pulvinar.
+      Turpis egestas maecenas pharetra convallis posuere morbi. Molestie at elementum eu facilisis sed odio morbi quis.
+    </p>
 
-          </div>
-          <div>
-            <p>"Citation"</p>
-          </div>
-        </section>
+    <section class="sect">
+      <article class="test">
+        <div>
+          <img class="img_mascotte" src="../assets/lopotichat_beau.svg">
+        </div>
+        <div>
+          <p> "Citation" </p>
+        </div>
       </article>
-
-      <p> Lorem ipsum </p>
-    </section>-->
+    </section>
 
   </div>
 
@@ -84,12 +79,7 @@ export default {
   }
 
   article p{
-    margin: 8px 5px;
-  }
-
-  .art{
-    display: flex;
-    justify-content: space-between;
+    margin: 12px 0;
   }
 
   .bloc{
@@ -99,24 +89,24 @@ export default {
 
   }
 
-  .test{
+  .test, .flex{
     display: flex;
     justify-content: space-between;
+  }
 
+  .sect{
     border-style: solid;
     border-radius: 12px;
-    border-color: white;
+    border-color: black;
 
-    padding: 10px 20px 10px 20px;
+    padding: 5px 15px 5px 15px;
     margin-top: 20px;
     margin-bottom: 20px;
   }
 
   .img_mascotte{
-    width: 20vw;
-    height: 20vw;
+    width: 15vw;
+    height: 25vh;
   }
-
-
 
 </style>
