cb98fcec4f73adf802435972986927dbfd4cf64b
BadiiiX <contact@mehdi-ali.me>
2022-12-02 07:43:00 +0100

    WHOLE PROJECT
    FUCK IIIIIIIIIT

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,56 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HomePage   from './components/HomePage.vue';
+import FirstPart  from './components/FirstPart.vue';
+import SecondPart from './components/SecondPart.vue';
+import ThirdPart  from './components/ThirdPart.vue';
+import FourthPart from "./components/FourthPart.vue";
+import Loader     from "./components/Loader.vue";
+import Jauge     from "./components/Jauge.vue";
+import {showFirstTimeElement}     from "./assets/localStorageManager";
+import AOS        from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
+
+const websiteVIH = 'https://lopotico.de/moche.html'
+
+
+let k              = [86, 73, 72],
+    n              = 0;
+document.onkeydown = (e) => {
+  console.log(e.keyCode);
+  if (e.keyCode === k[n++]) {
+    if (n === k.length) {
+      location.href = websiteVIH; // à remplacer par votre code
+      n             = 0;
+      return false;
+    }
+  } else {
+    n = 0;
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Loader v-if="showFirstTimeElement('loader')" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header>
+    <HomePage/>
   </header>
 
   <main>
-    <TheWelcome />
+    <FirstPart/>
+    <SecondPart/>
+    <ThirdPart/>
+    <FourthPart/>
+
+
   </main>
+
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
