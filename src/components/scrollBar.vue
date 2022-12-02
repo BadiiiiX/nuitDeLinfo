5b9b34fb306bdde8ac6e9252ab69567db6afef76
ATN04 <95452323+EtienneK2004@users.noreply.github.com>
2022-12-02 04:52:23 +0100

    Scroll bar et jauge

---
+++ src/components/scrollBar.vue
@@ -0,0 +1,17 @@
+<style>
+/* largueur */
+::-webkit-scrollbar {
+  width: 20px;
+}
+
+/* Scroll bar */
+::-webkit-scrollbar-track:hover {
+  box-shadow: inset 0 0 5px grey; 
+  border-radius: 10px;
+}
+ 
+/* Icon */
+::-webkit-scrollbar-thumb {
+  background: url("logo_sida_alternatif.svg") no-repeat center; 
+}
+</style>
